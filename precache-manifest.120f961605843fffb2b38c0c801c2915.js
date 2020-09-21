b039a1e8c1d2bd1cb93ec97563f1a3dfaaa003b6
Peter Wen <peter@peterwen.com>
2020-09-20 23:05:13 -0700

    Updates

---
+++ precache-manifest.120f961605843fffb2b38c0c801c2915.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfa63d1237ab534754fcbf515bc72dda",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9de20b6a3a396678337b",
+    "url": "/monsters-rolodex/static/css/main.44bcc782.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9de20b6a3a396678337b",
+    "url": "/monsters-rolodex/static/js/main.7b2db97f.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
